6f3f16b346364555511061f71d3d67a2fa09c729
Amy Madden <amadden@Amys-MacBook-Air.local>
2016-01-14 09:25:52 -0500

    Generate a unique user every time the api is called.  Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Demo Integration Server
 
+This project is a vertx 3 web server that mimics a vendor's server.  It takes a clientId
+and uses that to create a unique client to pass to the Lyric Registration API.  In a real life scenario,
+the clientId would be used to look up the client data and use that to pass to the api.
+
+    The Demo Integration Server is deployed at https://lyric-demo-server.herokuapp.com and the url to request an advance is https://lyric-demo-server.herokuapp.com/clients/:clientId/advance.
+
+This project can be forked and used to make modifications.  You can then deploy it to your own heroku
+instance.
+
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
--- src/main/java/com/lyric/DemoApi.java
+++ src/main/java/com/lyric/DemoApi.java
@@ -15,6 +15,7 @@ import org.apache.commons.lang3.EnumUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Set;
 import org.apache.commons.codec.binary.Base64;
 
@@ -38,11 +39,8 @@ public class DemoApi extends AbstractVerticle {
 
         router.route().handler(BodyHandler.create());
 
-        router.post("/members/:id/advance").handler(this::handleAdvanceRequest);
-        router.get("/test").handler(this::handleTest);
+        router.post("/clients/:id/advance").handler(this::handleAdvanceRequest);
 
-
-        //final JsonObject serverOptions = config().getJsonObject("serverOptions");
         final JsonObject serverOptions = new JsonObject();
         // Convert port to int
         serverOptions.put("port", Integer.parseInt(System.getenv("PORT"))).put("host", "0.0.0.0");
@@ -59,63 +57,95 @@ public class DemoApi extends AbstractVerticle {
 
     }
 
-    private void handleTest(RoutingContext routingContext){
-        JsonObject obj = new JsonObject().put("test", "yay");
-        routingContext.response().putHeader("content-type", "application/json").end(obj.encodePrettily());
-    }
-
     private void handleAdvanceRequest(RoutingContext routingContext) {
         logger.info("INTO HANDLE ADVANCE REQUEST");
         HttpServerResponse response = routingContext.response();
 
-        HttpClient client = vertx.createHttpClient(new HttpClientOptions(new JsonObject().put("defaultPort", 443).put("defaultHost", "api.lyricfinancial.com")).setSsl(true));
-        String userInfo = routingContext.getBodyAsString();
-        logger.info(userInfo);
+        String clientId = routingContext.request().getParam("id");
 
+        if(clientId == null){
+            response.setStatusMessage("Member Id cannot be null.");
+            response.setStatusCode(500).end();
+        }
 
-        HttpClientRequest request = client.post("/vendorAPI/v1/json/clients", resp -> {
+        HttpClient httpClient = vertx.createHttpClient(new HttpClientOptions(new JsonObject().put("defaultPort", 443).put("defaultHost", "api.lyricfinancial.com")).setSsl(true));
+
+        /* Look up member data from your system */
+        JsonObject client = findClient(clientId);
+
+        logger.info(client);
+
+        String contentType = routingContext.request().getHeader("content-type");
+
+        HttpClientRequest request = httpClient.post("/vendorAPI/v1/json/clients", resp -> {
             if (resp.statusCode() != 201) {
-                logger.info("GOT ERROR BACK");
-                logger.error(resp.statusMessage());
+                logger.error(String.format("An error occurred trying to register the member for an advance: %s", resp.statusMessage()));
                 response.setStatusMessage(resp.statusMessage());
                 response.setStatusCode(resp.statusCode()).end();
                 return;
             }
-            logger.info("SUCCESS");
+            /* The POST to the registration API returns an ACCESS_TOKEN in the header.  This header
+            is needed to pass back to the lyric-snippet so you need to send it back to your client.
+
+            Also, a memberToken is returned in the body.  This token should be saved to your database
+            so that any future calls will do updates to the system.
+             */
             JsonObject obj = new JsonObject().put("access_token", resp.getHeader("ACCESS_TOKEN"));
             response.putHeader("content-type", "application/json").end(obj.encodePrettily());
         });
 
-
-
+        /* 3 headers need to be set in order to call the Registration API.  vendorId, content-type
+        and authorization.  vendorId and the username and password to create the credentials will be
+        provided.  The content-type will depend on how the royalty data is being sent.
+         */
         request.putHeader("vendorId", "ascap");
-        request.putHeader("content-type", "application/json");
+        request.putHeader("content-type", contentType);
 
-        String auth = null;
+        /* Username and password are used to generate the authorization header.  These values need to
+        be base64 encoded to create the new authorization token.
+         */
+        String authToken = null;
         try {
-            auth = this.createCredentials("ascap", "WxjXgrzzGzrkPMv7hBFJ@PMkQX9e3e2N");
+            authToken = this.createCredentials("ascap", "WxjXgrzzGzrkPMv7hBFJ@PMkQX9e3e2N");
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            logger.error("Could not create client credentials", e.getCause());
         }
-        request.putHeader(HttpHeaders.AUTHORIZATION, "Basic " + auth);
+        request.putHeader(HttpHeaders.AUTHORIZATION, "Basic " + authToken);
 
-//        String authString = String.format("%s:%s", "ascap", "WxjXgrzzGzrkPMv7hBFJ@PMkQX9e3e2N");
-//        String bytesEncoded = Base64.getEncoder().encodeToString(authString.getBytes());
-//        request.putHeader("Authorization", String.format("Basic %s", bytesEncoded));
+        request.end(client.toString());
+    }
 
+    /* This gets a unique user every time for testing purposes.  This would really be a lookup from
+    your database.
+     */
+    private JsonObject findClient(String vendorClientAccountId){
 
-
-//        request.handler(resp -> {
-//            JsonObject obj = new JsonObject().put("access_token", resp.getHeader("access-token"));
-//            response.putHeader("content-type", "application/json").end(obj.encodePrettily());
-//        }).exceptionHandler(throwable -> {
-//            response.setStatusMessage(throwable.getMessage());
-//            response.setStatusCode(500).end();
-//        });
-
+        int START = 1000;
+        int END = 9999;
+        Random r = new Random();
+        int random = r.nextInt((END - START) + 1) + START;
 
 
-        request.end(userInfo);
+        JsonObject clientInfo = new JsonObject()
+                .put("firstName", String.format("Test%d", random))
+                .put("lastName", String.format("User%d", random))
+                .put("address1", "327 S 87 St")
+                .put("email", String.format("%s@email.com", vendorClientAccountId))
+                .put("city", "Omaha")
+                .put("state", "NE")
+                .put("zipCode", "68123")
+                .put("vendorClientAccountId", vendorClientAccountId)
+                .put("ssn", String.format("333-44-%d", random))
+                .put("phone", String.format("207555%d", random))
+                .put("mobilePhone", String.format("207556%d", random))
+                .put("bankName", "Bank of America")
+                .put("bankAccountNumber", "12345678")
+                .put("bankRoutingNumber", "211274450")
+                .put("bankAccountType", "checking")
+                .put("dob", "1967-01-01")
+                ;
+
+        return clientInfo;
     }
 
     private String createCredentials(String username, String password) throws UnsupportedEncodingException {
